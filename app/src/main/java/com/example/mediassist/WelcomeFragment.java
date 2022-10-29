511ee1469f8540c0a666dd83eb32fd89810ae964
your_username <manasa229@gmail.com>
2022-10-29 15:52:51 -0500

    naviagtion from welcome to signin page

--- app/src/main/java/com/example/mediassist/WelcomeFragment.java
+++ app/src/main/java/com/example/mediassist/WelcomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.mediassist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.mediassist.databinding.FragmentFirstBinding;
+import com.example.mediassist.login.LoginActivity;
 
 public class WelcomeFragment extends Fragment {
 
@@ -22,6 +25,16 @@ public class WelcomeFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        Button signInButton = binding.signinButtonView;
+        signInButton.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                                                startActivity(intent);
+                                            }
+                                        }
+
+        );
         return binding.getRoot();
 
     }
